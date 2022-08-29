c42fec3292c1255e543945f9e393bbf5b923af23
Viviane Perrotti <viperrotti@gmail.com>
2022-08-29 17:50:40 -0300

    first commit

---
+++ testeAPI/Cliente.cs
@@ -0,0 +1,27 @@
+﻿namespace testeAPI
+{
+    public class Cliente
+    {
+        public DateTime DataNascimento { get; set; }
+
+        public int Cpf { get; set; }
+
+        public int Idade => ObterIdade();
+
+        public string? Nome { get; set; }
+
+
+        public int ObterIdade()
+        {
+            int idade = DateTime.Now.Year - DataNascimento.Year;
+            if (DateTime.Now.DayOfYear < DataNascimento.DayOfYear)
+            {
+                idade--;
+            }
+            return idade;
+        }
+    }
+
+
+
+}
---
+++ testeAPI/Controllers/ClienteController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace testeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        private static readonly string[] nomes = new[]
+        {
+        "Renato", "Marcela", "Mariana", "Fernanda", "Rodrigo", "Ricardo", "João Carlos", "Viviane", "Victor"
+    };
+
+        private readonly ILogger<ClienteController> _logger;
+
+        public List<Cliente> cadastros { get; set; }
+        public ClienteController(ILogger<ClienteController> logger)
+        {
+            _logger = logger;
+            cadastros = Enumerable.Range(1, 5).Select(index => new Cliente
+            {
+                DataNascimento = DateTime.Now.AddDays(-Random.Shared.Next(6000, 20000)),
+                Cpf = Random.Shared.Next(000000000, 999999999),
+                Nome = nomes[Random.Shared.Next(nomes.Length)]
+            })
+                .ToList();
+        }
+
+        [HttpGet]
+        public List<Cliente> Consult(int index)
+        {
+            return cadastros;
+        }
+
+        [HttpPost]
+        public Cliente Insert(Cliente cadastro)
+        {
+            cadastros.Add(cadastro);
+            return cadastro;
+        }
+
+        [HttpPut]
+        public Cliente Atualizar(int index, Cliente cadastro)
+        {
+            cadastros[index] = cadastro;
+            return cadastros[index];
+        }
+
+        [HttpDelete]
+        public List<Cliente> Deletar(int index)
+        {
+            cadastros.RemoveAt(index);
+            return cadastros;
+        }
+    }
+}
