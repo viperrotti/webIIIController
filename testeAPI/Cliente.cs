9e6aeda20976f798a6ca1cf3e933cbc4ae14c809
Viviane Perrotti <viperrotti@gmail.com>
2022-08-31 14:13:40 -0300

    Decorators

--- testeAPI/Cliente.cs
+++ testeAPI/Cliente.cs
@@ -1,20 +1,33 @@
-﻿namespace testeAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace testeAPI
 {
     public class Cliente
     {
-        public DateTime DataNascimento { get; set; }
+        [Required(ErrorMessage = "Data é obrigatório")]
+        public DateTime? DataNascimento { get; set; }
 
-        public int Cpf { get; set; }
+
+
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "CPF deve conter 11 dígitos")]
+        [Required(ErrorMessage = "CPF é obrigatório")]
+        public string? Cpf { get; set; }
 
         public int Idade => ObterIdade();
 
+        [MinLength(1, ErrorMessage = "Nome não pode ser vazio")]
+        [Required(ErrorMessage = "Nome é obrigatório")]
         public string? Nome { get; set; }
 
 
         public int ObterIdade()
         {
-            int idade = DateTime.Now.Year - DataNascimento.Year;
-            if (DateTime.Now.DayOfYear < DataNascimento.DayOfYear)
+            if(!DataNascimento.HasValue)
+            {
+                return -1;
+            }
+            int idade = DateTime.Now.Year - DataNascimento.Value.Year;
+            if (DateTime.Now.DayOfYear < DataNascimento.Value.DayOfYear)
             {
                 idade--;
             }
--- testeAPI/Controllers/ClienteController.cs
+++ testeAPI/Controllers/ClienteController.cs
@@ -10,6 +10,10 @@ namespace testeAPI.Controllers
         {
         "Renato", "Marcela", "Mariana", "Fernanda", "Rodrigo", "Ricardo", "João Carlos", "Viviane", "Victor"
     };
+        private static readonly string[] cpfs = new[]
+        {
+        "32699877433", "65887799633", "57899968544", "22588465899", "33477851298", "85744695322", "54899632577", "55123659988", "32899564722"
+    };
 
         private readonly ILogger<ClienteController> _logger;
 
@@ -20,37 +24,68 @@ namespace testeAPI.Controllers
             cadastros = Enumerable.Range(1, 5).Select(index => new Cliente
             {
                 DataNascimento = DateTime.Now.AddDays(-Random.Shared.Next(6000, 20000)),
-                Cpf = Random.Shared.Next(000000000, 999999999),
+                Cpf = cpfs[Random.Shared.Next(cpfs.Length)],
                 Nome = nomes[Random.Shared.Next(nomes.Length)]
             })
                 .ToList();
         }
 
         [HttpGet]
-        public List<Cliente> Consult(int index)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<Cliente>> Consultar()
         {
-            return cadastros;
+            return Ok(cadastros);
         }
 
-        [HttpPost]
-        public Cliente Insert(Cliente cadastro)
+        [HttpGet("/cadastro/{index}/consultar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<List<Cliente>> Consultar2(int index)
+        {
+            if (index > cadastros.Count)
+            {
+                return NotFound();
+            }
+            return Ok(cadastros[index]);
+        }
+
+
+        [HttpPost("/cadastro/cadastrar")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public ActionResult <Cliente> Inserir([FromBody]Cliente cadastro)
         {
             cadastros.Add(cadastro);
-            return cadastro;
+            return StatusCode(201, cadastro);
         }
 
-        [HttpPut]
-        public Cliente Atualizar(int index, Cliente cadastro)
+        [HttpPut("/cadastro/{index}/atualizar")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Atualizar(int index, Cliente cadastro)
         {
+            if(index >= cadastros.Count || index < 0)
+            {
+                return NotFound();
+            }
             cadastros[index] = cadastro;
-            return cadastros[index];
+            return Ok(cadastros[index]);
         }
 
-        [HttpDelete]
-        public List<Cliente> Deletar(int index)
+
+        [HttpDelete("/cadastro/{index}/deletar")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Deletar(int index)
         {
+            if(index >= cadastros.Count || index < 0)
+            {
+                return NotFound();
+            }
             cadastros.RemoveAt(index);
-            return cadastros;
+            return NoContent();
         }
     }
 }
